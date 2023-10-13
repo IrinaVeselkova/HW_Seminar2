9ee6632326c1e2c1b8306d8197dba56a307768f0
Veselkova Irina <veselkovaiv@yandex.ru>
2023-10-13 10:24:54 +0300

    Ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,27 @@
+﻿int ReadInt(string msg)// объявляем функцию msg строковую
+{
+Console.WriteLine(msg);//пишет на экран параметр msg
+string a = Console.ReadLine();
+int numb;
+numb = int.Parse(a);//преобразуем строку в число
+return numb;// возврат из функции
+}
+int numb = ReadInt("Введите трехзначное число:");
+
+if (100 > numb ^ numb > 999)
+{
+    System.Console.WriteLine($"Пожалуйста, введите треххначное число. Число {numb} не трехзначное");
+
+}
+else 
+
+{
+    int i = 100;
+    while (i < numb)
+    {
+        numb = numb/10;
+        i++;
+    }
+    Console.WriteLine(numb%10);
+}    
+
