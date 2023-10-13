c31097809f4f68dfa8866e27bafdfb344ee6b83b
Veselkova Irina <veselkovaiv@yandex.ru>
2023-10-13 10:37:11 +0300

    Ex32

---
+++ ex3/Program.cs
@@ -0,0 +1,23 @@
+﻿int ReadInt(string msg)// объявляем функцию msg строковую
+{
+Console.WriteLine(msg);//пишет на экран параметр msg
+string a = Console.ReadLine();
+int numb;
+numb = int.Parse(a);//преобразуем строку в число
+return numb;// возврат из функции
+}
+int numb = ReadInt("Введите число от 1 до 7:");
+
+if (numb <1 ^ numb >7)
+{
+    System.Console.WriteLine("Необходимо ввести число от 1 до 7");
+}
+else 
+{
+    if (numb == 6 || numb == 7)
+    {
+        Console.WriteLine($"Ура!!! Сегодня выходной!");
+    }
+    else Console.WriteLine($"К сожалению, сегодня не выходной! Пора на работу:(");   
+}
+
