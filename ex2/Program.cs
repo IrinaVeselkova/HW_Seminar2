7fa601cb9585e1c35a28d21a7818b4b31e38d344
Veselkova Irina <veselkovaiv@yandex.ru>
2023-10-13 10:28:51 +0300

    Ex2

---
+++ ex2/Program.cs
@@ -0,0 +1,27 @@
+﻿int ReadInt(string msg)// объявляем функцию msg строковую
+{
+Console.WriteLine(msg);//пишет на экран параметр msg
+string a = Console.ReadLine();
+int numb;
+numb = int.Parse(a);//преобразуем строку в число
+return numb;// возврат из функции
+}
+int numb = ReadInt("Введите число:");
+
+if (100 > numb )
+{
+    System.Console.WriteLine($"В числе {numb} нет третьего знака.");
+
+}
+else 
+
+{
+    int i = 1000;
+    while (i < numb)
+    {
+        numb = numb/10;
+        i++;
+    }
+    Console.WriteLine(numb%10);
+}    
+
